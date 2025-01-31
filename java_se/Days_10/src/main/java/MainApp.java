3ca358aa56f51adac1177394279d5b6c68e7868c
Hakan ÖZER <hakanozer02@gmail.com>
2025-01-31 20:01:01 +0300

    Days_10

--- java_se/Days_10/src/main/java/MainApp.java
+++ java_se/Days_10/src/main/java/MainApp.java
@@ -6,6 +6,13 @@ public class MainApp {
 
         CustomerService customerService = new CustomerService();
 
+        // Data ekleme
+        // int addStatus = customerService.addCustomer("Kasım", "Duyar", "kasim@mail.com","12345");
+        // System.out.println("Add status: " + addStatus);
+
+        // Data Çekme
+        customerService.allCustomer();
+
     }
 
 
--- java_se/Days_10/src/main/java/services/CustomerService.java
+++ java_se/Days_10/src/main/java/services/CustomerService.java
@@ -3,6 +3,9 @@ package services;
 import utils.DB;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerService {
 
@@ -20,6 +23,50 @@ public class CustomerService {
             pre.executeUpdate();
         }catch (Exception ex) {
             System.err.println("DB Error: " + ex.getMessage());
+        }finally {
+            db.close();
+        }
+    }
+
+    // insert -> data ekleme
+    public int addCustomer( String name, String surname, String email, String password ) {
+        DB db = new DB();
+        int status = 0;
+        try {
+            String sql = "insert into customer(name,surname,email,password) values(?,?,?,?)";
+            PreparedStatement pre = db.connect().prepareStatement(sql);
+            pre.setString(1, name);
+            pre.setString(2, surname);
+            pre.setString(3, email);
+            pre.setString(4, password);
+            status = pre.executeUpdate(); // delete - update - insert
+        }catch (Exception ex) {
+            System.err.println("addCustomer error: " + ex.getMessage());
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+
+    public void allCustomer() {
+        DB db = new DB();
+        List<String> ls = new ArrayList<>();
+        try {
+            String sql = "select * from customer";
+            PreparedStatement pre = db.connect().prepareStatement(sql);
+            ResultSet rs = pre.executeQuery();
+            while( rs.next() ) {
+               // satır içindeki tüm bilgiler bu bölümdedir.
+               int cid = rs.getInt("cid");
+               String name = rs.getString("name");
+               String surname = rs.getString("surname");
+               String email = rs.getString("email");
+               String password = rs.getString("password");
+               System.out.println(cid + " " + name + " " + surname + " " + email + " " + password);
+            }
+        }catch (Exception ex) {
+            System.err.println("allCustomer error: " + ex.getMessage());
         }finally {
             db.close();
         }
