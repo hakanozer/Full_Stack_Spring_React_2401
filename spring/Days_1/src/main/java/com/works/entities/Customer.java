48da3a4c706374c807922bfb20c1c24a0934c3c1
Hakan ÖZER <hakanozer02@gmail.com>
2025-02-12 20:27:48 +0300

    Update Customer.java

--- spring/Days_1/src/main/java/com/works/entities/Customer.java
+++ spring/Days_1/src/main/java/com/works/entities/Customer.java
@@ -1,10 +1,9 @@
 package com.works.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Entity // Customer bir veritabanı tablosudur.
 @Data
@@ -14,10 +13,37 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cid;
 
+    @Length(min = 2, max = 20)
+    @NotNull // nesne varlığını - null check
+    @NotEmpty // data varlığını - equals("") check
     private String name;
+
+    @Size(min = 2, max = 20)
+    @NotEmpty
+    @NotNull
     private String surname;
+
+    @Column(unique = true)
+    @Email
+    @Size(min = 6, max = 50)
+    @NotEmpty
+    @NotNull
     private String email;
+
+
+    // 8 char
+    // )(/&%+^
+    // A-Z
+    // a-z
+    // 1-9
+    @Size(min = 8, max = 16)
+    @NotEmpty
+    @NotNull
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]{8,}$")
     private String password;
+
+    @Max(50)
+    @Min(18)
     private Integer age;
 
 }
