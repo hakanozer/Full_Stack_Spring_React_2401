98d46111a3f9db31d2df961b1c2b4d18afb981f3
Hakan ÖZER <hakanozer02@gmail.com>
2025-02-19 20:55:36 +0300

    Date Format

--- spring/ProductManager/src/main/java/com/works/entities/Product.java
+++ spring/ProductManager/src/main/java/com/works/entities/Product.java
@@ -4,6 +4,7 @@ import com.works.utils.EProduct;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -21,7 +22,7 @@ public class Product {
     @NotNull
     private String title;
 
-    @Size(message = "3", min = 500)
+    @Size(min = 3, max = 500)
     @NotEmpty
     @NotNull
     private String description;
@@ -38,6 +39,8 @@ public class Product {
     @Size(min = 6, max = 6)
     private String color;
 
+    // 19-02-2025 20:52:22
+    @DateTimeFormat(pattern = "YYYY")
     private Date date;
 
 
---
+++ spring/ProductManager/src/main/java/com/works/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.works.services;
+
+import com.works.entities.Product;
+import com.works.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    final ProductRepository productRepository;
+
+    public Product save( Product product ) {
+        Date date = new Date();
+        product.setDate( date );
+        productRepository.save(product); // setID ?
+        return product;
+    }
+
+    public List<Product> all() {
+        List<Product> ls = productRepository.findAll();
+        return ls; // select * from product
+    }
+
+}
