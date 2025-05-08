33e6abbd40d596f06b6134383e616dbff99dda2a
Hakan ÖZER <hakanozer02@gmail.com>
2025-05-08 20:07:52 +0300

    Data Manager - JWT Start

---
+++ spring/DataManager-JWT/src/main/java/com/works/configs/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.works.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf( csrf -> csrf.disable() )
+                .formLogin( formLogin -> formLogin.disable() )
+                .build();
+    }
+
+
+}
+
+/*
+ali01 -> product
+veli01 -> note
+zehra01 -> product, note
+ */
---
+++ spring/DataManager-JWT/src/main/java/com/works/configs/SecurityFilter.java
@@ -0,0 +1,20 @@
+package com.works.configs;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Configuration
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+
+}
