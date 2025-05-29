8390cfc7ee4cb6b7c10c1f9f3d16fafefdfca9ec
Hakan ÖZER <hakanozer02@gmail.com>
2025-05-28 18:50:52 -0700

    React Component

--- frontEnd/project/src/index.tsx
+++ frontEnd/project/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Login from './pages/Login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<div>merhaba</div>);
+root.render(<Login />);
