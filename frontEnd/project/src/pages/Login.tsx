7bc3894e14bcff5853a6497d39128e50dbd0454c
Hakan ÖZER <hakanozer02@gmail.com>
2025-05-28 20:33:27 -0700

    Form Send

--- frontEnd/project/src/pages/Login.tsx
+++ frontEnd/project/src/pages/Login.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
 function Login() {
+
+  // arrow func
+  const userLogin = (evt: React.FormEvent) => {
+    evt.preventDefault()
+    console.log("userLogin call")
+  }
+
   return (
-    <div>Login</div>
+    <>
+        <div className="row">
+          <div className='col-12 col-md-3 col-lg-4'></div>
+          <div className='col-12 col-md-6 col-lg-4'>
+            <h2>User Login</h2>
+            <form onSubmit={userLogin}>
+              <div className='mb-2'>
+                <input required type='email' placeholder='E-Mail' className='form-control' />
+              </div>
+              <div className='mb-2'>
+                <input required type='password' placeholder='Password' className='form-control' />
+              </div>
+              <div className='d-flex justify-content-between'>
+                <button className='btn btn-success'>Login</button>
+                <a className='btn btn-dark' href=''>Register</a>
+              </div>
+            </form>
+          </div>
+          <div className='col-12 col-md-3 col-lg-4'></div>
+        </div>
+    </>
   )
 }
 
