6364e5c3bfe9811f4a47c61ba7c82a8c1951f2c4
Hakan ÖZER <hakanozer02@gmail.com>
2025-07-10 20:24:47 +0300

    Api Gateway

---
+++ microservices/Product/src/main/java/com/works/configs/FilterConfig.java
@@ -0,0 +1,28 @@
+package com.works.configs;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class FilterConfig implements Filter {
+
+    @Value("${gateway.ip}")
+    private String gatewayIp;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        System.out.println("Product Call");
+        String ip = request.getRemoteAddr();
+        if (ip.equals(gatewayIp)) {
+            filterChain.doFilter(request,response);
+        }
+    }
+
+}
